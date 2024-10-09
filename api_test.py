f59b55feae1e68747d77b0960d60445563210edc
abramovsergeyn <abramovsergeyn@gmail.com>
2024-10-09 18:05:23 +0300

    api_test.py
    api_test.py

--- api_test.py
+++ api_test.py
@@ -8,7 +8,7 @@ class Test(unittest.TestCase):
     def test_get_gmt(self):
         result = requests.get('http://localhost:8080/')
         response_body = str(result.content)
-        self.assertIn('GMT', response_body)
+        self.assertIn('GMT', response_body)    
     def test_get_moscow(self):
         result = requests.get('http://localhost:8080/Europe/Moscow')
         response_body = str(result.content)
@@ -23,10 +23,10 @@ class Test(unittest.TestCase):
         params = ""
         result = requests.post('http://localhost:8080/api/v1/convert', params=params, headers=headers, data=data)
         response_body = str(result.content)
-        self.assertIn('Invalid input data', response_body)
+        self.assertIn('Invalid input data', response_body)    
     def test_post_invalid_diffdate(self):
         headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-        data = '{"first_date": "10.10.2024 10:10:00", "first_tz": "PPP","second_date": "11:00am 2024-10-10", "second_tz": "Europe/Moscow"}'
+        data = '{"first_date": "12.06.2024 22:21:05", "first_tz": "PPP","second_date": "12:30pm 2024-02-01", "second_tz": "Europe/Moscow"}'
         params = ""
         result = requests.post('http://localhost:8080/api/v1/datediff', params=params, headers=headers, data=data)
         response_body = str(result.content)
@@ -34,17 +34,17 @@ class Test(unittest.TestCase):
     def test_invalid_post_path(self):
         result = requests.post('http://localhost:8080/api/v1/qwerty')
         response_body = str(result.content)
-        self.assertIn('Unknown request', response_body)
+        self.assertIn('Unknown request', response_body)        
     def test_post_convert(self):
         headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-        data = '{"date": "10.10.2023 10:10:00", "tz": "EST"}'
+        data = '{"date": "12.20.2021 22:21:05", "tz": "EST"}'
         params = ""
         result = requests.post('http://localhost:8080/api/v1/convert/GMT', params=params, headers=headers, data=data)
         response_body = str(result.content)
         self.assertIn('GMT', response_body)
     def test_post_datediff(self):
         headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-        data = '{"first_date": "10.10.2024 10:10:00", "first_tz": "EST","second_date": "11:00am 2024-10-10", "second_tz": "Europe/Moscow"}'
+        data = '{"first_date": "12.06.2024 22:21:05", "first_tz": "EST","second_date": "12:30pm 2024-02-01", "second_tz": "Europe/Moscow"}'
         params = ""
         result = requests.post('http://localhost:8080/api/v1/datediff', params=params, headers=headers, data=data)
         response_body = str(result.content)
